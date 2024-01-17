57f1564b4efbc91881fd44f74148fefaef3c858d
samanthfel <143027804+samanthfel@users.noreply.github.com>
2024-01-17 12:16:18 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -13,7 +13,7 @@ public class GoogleBillboard {
 	
 
 	//Finish this function
-	public staticnboolean isPrime(double dNum){
+	public static boolean isPrime(double dNum){
 		for(int i=2; i<= Math.sqrt(dNum);i++){
       			if (dNum%i == 0){
         			return false;
